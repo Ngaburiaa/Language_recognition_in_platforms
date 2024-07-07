248e3170eccddb05d636490c38a7a9d937443650
ALAMIN JUMA MAGOTI <61662333+Alamin-Juma@users.noreply.github.com>
2024-07-07 15:42:50 +0300

    Revert "Language recognitions"

--- DetectLanguage/src/Components/inputComponent.jsx
+++ DetectLanguage/src/Components/inputComponent.jsx
@@ -1,16 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import ElementsComponents from "./ElementsComponent/ElementsComponents";
 import AudioRecorder from "./speechComponent";
 
-function InputComponent() {
-  const [transcript, setTranscript] = useState("");
 
-  return (
-    <div className="inputFiled">
-      <ElementsComponents transcript={transcript} />
-      <AudioRecorder transcript={transcript} setTranscript={setTranscript} />
-    </div>
-  );
+function InputComponent() {
+  return <div className="inputFiled">
+    <ElementsComponents/>
+    <AudioRecorder/>
+   
+  </div>;
 }
 
 export default InputComponent;
